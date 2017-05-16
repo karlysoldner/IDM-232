7803595ee1c518899e99d5c40b7fad9f4cf2c5bb
Karly <karlysoldner@Karlys-MacBook-Air.local>
2017-05-15 22:10:40 -0400

    all it does is show the whole database at the top of the page but it works ¯\_(ツ)_/¯

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="stylesheet" href="stylesheet.css">
+    
+    <link rel="stylesheet" type="text/css"
+      href="https://fonts.googleapis.com/css?family=Rubik">
+<link rel="stylesheet" type="text/css"
+      href="https://fonts.googleapis.com/css?family=Dosis">
+
+
+    <link rel="stylesheet" type="text/css" href="//weloveiconfonts.com/api/?family=brandico">
+    <title>About Me</title>
+</head>
+
+<body>
+<?php include 'untitled.php';?>
+
+    <div class="wrapper">
+        <header>
+            <div class="flex-parent">
+                <div class="flex-child">
+                    <h1>Karly Soldner</h1>
+                </div>
+
+                <div class="flex-child">
+                    <nav>
+                        <ul>
+                            <li><a href="index.html">About Me</a></li>
+                            <li><a href="projects.html">Projects</a></li>
+                            <li><a href="resume.html">Resume</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+            </div>
+        </header>
+    </div>
+
+    <h1> About Me </h1>
+    <div class="col-6">
+        <img src="karly.png" alt="Karly, smiling and possibly even cute" style="max-height: 35rem">
+    </div>
+
+    <div class="col-6">
+        <p>I'm Karly. I'm a senior at Drexel studying environmental science, biology, and interactive digital media. Talk to me about camping, traveling, living abroad, science, technology, memes, politics, hiring me, Kurt Vonnegut, and Philadelphia.</p>
+
+        <ul style="list-style-type: none">
+            <li class=" social-button no-hover" style="float: left;">
+                <a href="https://www.linkedin.com/in/karlysoldner" target="_blank" class="brandico-linkedin"></a>
+            </li>
+            <li class=" social-button no-hover" style="float: left;">
+                <a href="https://www.instagram.com/karlymoon999/" target="_blank" class="brandico-instagram"></a>
+            </li>
+            <li class=" social-button no-hover" style="float: left;">
+                <a href="https://twitter.com/karlysoldner" target="_blank" class="brandico-twitter-bird"></a>
+            </li>
+            <li class=" social-button no-hover" style="float: left;">
+                <a href="https://www.facebook.com/karly.soldner" target="_blank" class="brandico-facebook"></a>
+            </li>
+        </ul>
+    </div>
+
+  
+</body>
+
+</html>
---
+++ untitled.php
@@ -0,0 +1,58 @@
+<?php
+	//thanks Phil
+  // Step 1: Create Database Connection
+  $dbhost = "localhost";
+  $dbuser = "root";
+  $dbpass = "root";
+  $dbname = "Projects";
+  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+  // Check the connection is good with no errors
+  if (mysqli_connect_errno()) {
+    die ("Database connection failed: " .
+      mysqli_connect_error() .
+      " (" . mysqli_connect_errno() . ")"
+    );
+  }
+  // Step 2: Preform Database Query
+  $query = "SELECT * FROM projects";
+  $result = mysqli_query($connection, $query);
+  // Check there are no errors with our SQL statement
+  if (!$result) {
+    die ("Database query failed.");
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Databases</title>
+</head>
+<body>
+
+  <?php
+    // Step 3: Use Returned Data
+    echo "<pre>";
+    while ($row = mysqli_fetch_row($result)) {
+      var_dump($row);
+      echo "<hr>";
+    }
+    echo "</pre>";
+  ?>
+
+  <?php
+    // Step 4: Release Returned Data
+    mysqli_free_result($result);
+  ?>
+
+  <!--
+  We could setup a new query here and build soemthing else if needed.
+  We've freed the previous result, so we're able to build something
+  else if needed.
+  -->
+
+  <?php
+    // Step 5: Close Database Connection
+    mysqli_close($connection);
+  ?>
+</body>
+</html>
